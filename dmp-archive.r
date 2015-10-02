c4a85db57427ec2a449e30d5e10cb4ac1a879de5
rwmoore <rwmoore@renci.org>
2015-10-01 22:28:47 -0400

    Removed flag from createLogFile

--- dmp-archive.r
+++ dmp-archive.r
@@ -11,7 +11,7 @@ archiveRule {
   msiGetSystemTime(*TimeA, "unix");
 
 #============ create a collection for log files if it does not exist ===============
-  createLogFile (*Dest, "log", "Check", *Res, *LPath, *Lfile, "forceFlag=", *L_FD);
+  createLogFile (*Dest, "log", "Check", *Res, *LPath, *Lfile, *L_FD);
 
 #============ find files to archive
   *Query = select DATA_NAME, DATA_CHECKSUM,COLL_NAME where COLL_NAME like '*Src%';
